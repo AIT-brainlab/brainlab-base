3154ea5a9a6d1871f9df3cb26165e0ed69091362
akraradets <akraradets@gmail.com>
2026-09-13 22:31:25 +0700

    feat(dns): add DPI `base` NS subdelegation record for domain management

--- mgmt/terraform/foundation/dns.tf
+++ mgmt/terraform/foundation/dns.tf
@@ -106,6 +106,20 @@ resource "google_dns_record_set" "dpi_sandbox_ns" {
   ]
 }
 
+# DPI `base` Subdelegation (NS)
+resource "google_dns_record_set" "dpi_base_ns" {
+  name         = "base.dpi.ait.ac.th."
+  managed_zone = google_dns_managed_zone.dpi_zone.name
+  type         = "NS"
+  ttl          = 300
+  rrdatas = [
+    "ns-cloud-e1.googledomains.com.",
+    "ns-cloud-e2.googledomains.com.",
+    "ns-cloud-e3.googledomains.com.",
+    "ns-cloud-e4.googledomains.com."
+  ]
+}
+
 # Observation Cluster Subdelegation: obs.dpi.ait.ac.th (NS)
 # Awaiting zone creation in dedicated observation GCP project.
 # Replace placeholders with the 4 nameservers provided by the observation project's Cloud DNS.
